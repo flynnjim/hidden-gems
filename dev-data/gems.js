2534fc8ee911e02917b6243ea13aaec5562aa0a6
Emily <e.spiers13@gmail.com>
2024-09-27 15:55:51 +0100

    dev data

---
+++ dev-data/gems.js
@@ -0,0 +1,191 @@
+const gems = [
+  {
+    title: "Cultural Heritage Festival",
+    description:
+      "A celebration of diverse cultural traditions through music, dance, and art.",
+    category: "culture",
+    img_url: [
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fculture-festival.jpg?alt=media&token=3ccefa14-e774-4f77-b39b-ca08e15e23dd",
+    ],
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
+    address: "Piccadilly Gardens, Manchester M1 1RN, United Kingdom",
+    date: "2024-10-15T14:00:00",
+    type: "event",
+  },
+  {
+    title: "Art in the Park",
+    description:
+      "Local artists display their work in an open park setting with live music and refreshments.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,art%2Din%2Dthe%2Dpark.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
+    address: "Piccadilly Gardens, Manchester M1 1RN, United Kingdom",
+    date: "2024-10-02T11:00:00",
+    type: "event",
+  },
+  {
+    title: "Street Food Festival",
+    description:
+      "Sample a variety of street food from local and international vendors.",
+    category: "food",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,street%2Dfood.jpeg,-Size",
+    ],
+    location: {
+      latitude: 51.5074,
+      longitude: -0.1278,
+    },
+    address: "Albert Square, Manchester M2 5DB, United Kingdom",
+    date: "2024-09-30T12:00:00",
+    type: "event",
+  },
+  {
+    title: "Foraging Adventure",
+    description:
+      "Join a foraging expert on a walk through nature, learning about what you can eat from nature!",
+    category: "nature",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,foraging%2Dadventure.webp,-Size",
+    ],
+    location: {
+      latitude: 53.4693,
+      longitude: -2.2672,
+    },
+    address: "Fletcher Moss Park, Didsbury, Manchester M20 2SW, United Kingdom",
+    date: "2024-10-05T08:00:00",
+    type: "event",
+  },
+  {
+    title: "Gourmet Tasting Night",
+    description:
+      "An exclusive evening of gourmet food tastings from local chefs.",
+    category: "food",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,gormet%2Dtasting.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4783,
+      longitude: -2.244,
+    },
+    address: "The Ivy, Hardman St, Manchester M3 3EB, United Kingdom",
+    date: "2024-10-12T19:00:00",
+    type: "event",
+  },
+  {
+    title: "City Lights Photography Tour",
+    description:
+      "Capture stunning photos of the city at night with photography expert guidance.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,city%2Dlights.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
+    address: "Northern Quarter, Manchester M4 1BD, United Kingdom",
+    date: "2024-09-29T18:30:00",
+    type: "event",
+  },
+  {
+    title: "World Cuisine Cooking Class",
+    description:
+      "Learn how to cook dishes from around the world with local chefs.",
+    category: "food",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,world%2Dcuisine.webp,-Size",
+    ],
+    location: {
+      latitude: 53.4832,
+      longitude: -2.2241,
+    },
+    address:
+      "Cookery School, St Peter's Square, Manchester M2 3AE, United Kingdom",
+    date: "2024-10-08T15:00:00",
+    type: "event",
+  },
+  {
+    title: "The Secret Garden Cafe",
+    description:
+      "I stumbled upon this little gem while wandering around the neighborhood. The garden patio is such a peaceful escape from the city, and the coffee? Absolutely the best I've had in a long time! The pastries are homemade and pair perfectly with their artisan brews. I'll definitely be back to enjoy more quiet mornings here.",
+    category: "food",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,secret%2Dgarden.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4654,
+      longitude: -2.2367,
+    },
+    address: "West Didsbury, Manchester M20 2BA, United Kingdom",
+    type: "place",
+  },
+  {
+    title: "Moonlit Rooftop Bar",
+    description:
+      "What an amazing find! This rooftop bar is the perfect place to unwind after a busy day. The craft cocktails are incredible, and the view of the city skyline at night is simply breathtaking. It's got a relaxed vibe, and the bartenders were super friendly. A must-visit if you're in the area.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rooftop%2Dbar.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4783,
+      longitude: -2.244,
+    },
+    address:
+      "Hardman Square, Spinningfields, Manchester M3 3EB, United Kingdom",
+    type: "place",
+  },
+  {
+    title: "The Hidden Lantern",
+    description:
+      "This place blew me away! Tucked behind an unassuming bookstore, The Hidden Lantern is a speakeasy like no other. The ambiance is intimate, and the cocktails are unique and carefully crafted. If you love discovering secret spots, you have to check this out. It's like stepping into a different world.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,hidden%2Dlantern.jpeg,-Size",
+    ],
+    location: {
+      latitude: 53.4811,
+      longitude: -2.2433,
+    },
+    address: "Royal Exchange, Manchester M2 7EA, United Kingdom",
+    type: "place",
+  },
+  {
+    title: "Vinyl Dreams Record Shop",
+    description:
+      "This record store is a dream for any music lover! It's a little tucked away, but once you find it, you'll want to stay for hours. They have a great collection of rare vinyls, and the staff is super knowledgeable and friendly. I found some real treasures here, and I can't wait to come back!",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,vinyl%2Ddreams.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4712,
+      longitude: -2.248,
+    },
+    address: "Smithfield Market, Manchester M4 1BN, United Kingdom",
+    type: "place",
+  },
+  {
+    title: "Rustic Roots Farm Market",
+    description:
+      "I came across this charming little farm stand on a weekend drive, and it's now my favorite spot for fresh produce. Everything is organic, and you can tell it's grown with love. The homemade jams are also incredible - I can't leave without grabbing a jar or two!",
+    category: "nature",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rustic%2Droots.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4536,
+      longitude: -2.2684,
+    },
+    address: "Lymm, Manchester WA13 0HS, United Kingdom",
+    type: "place",
+  },
+];
---
+++ dev-data/users.js
@@ -0,0 +1,103 @@
+const users = [
+  [
+    {
+      name: "John Doe",
+      username: "johndoe123",
+      email: "johndoe@example.com",
+      password: "P@ssw0rd123!",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FJohn%20Doe.png?alt=media&token=c19e3cb9-6890-498d-b0be-4ec63e24ed11",
+      user_type: "regular",
+    },
+    {
+      name: "Jane Smith",
+      username: "janesmith456",
+      email: "janesmith@example.com",
+      password: "S3cur3P@ss!",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FJane%20Smith.png?alt=media&token=395e8115-f077-40a8-9731-e2cb44641db9",
+      user_type: "artist",
+    },
+    {
+      name: "Michael Brown",
+      username: "mikebrown789",
+      email: "mikebrown@example.com",
+      password: "Browny2024#",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FMichael%20Brown.png?alt=media&token=f6821d60-6fd6-4f86-9f81-f7c5ce480898",
+      user_type: "regular",
+    },
+    {
+      name: "Emily Davis",
+      username: "emilyd202",
+      email: "emilydavis@example.com",
+      password: "Eml!DavS9987",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FEmily%20Davis.png?alt=media&token=6ebd67b9-ac94-4323-94c5-5c9390f886ea",
+      user_type: "artist",
+    },
+    {
+      name: "David Wilson",
+      username: "davidw_88",
+      email: "davidwilson@example.com",
+      password: "Wilz0n88!",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FDavid%20Wilson.png?alt=media&token=e2f6cfbb-422e-4e7b-92c2-391a54b13e3a",
+      user_type: "regular",
+    },
+    {
+      name: "Laura Johnson",
+      username: "laurajohn01",
+      email: "lauraj@example.com",
+      password: "L@ur4JPass",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FLaura%20Johnson.png?alt=media&token=28210b21-83d1-4877-b8a7-ce56073c02c3",
+      user_type: "artist",
+    },
+    {
+      name: "Chris Martinez",
+      username: "chrismart95",
+      email: "chrism@example.com",
+      password: "Chr!sM@rt9",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FChris%20Martinez.png?alt=media&token=9dc51f76-7c65-4039-b456-0345e471afa6",
+      user_type: "regular",
+    },
+    {
+      name: "Olivia Lee",
+      username: "olivialee22",
+      email: "olivialee@example.com",
+      password: "Olivi@P@ss22",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FOlivia%20Lee.png?alt=media&token=eac9c4fd-7312-4bf7-95d7-3e616b409cca",
+      user_type: "regular",
+    },
+    {
+      name: "James Clark",
+      username: "jamesc890",
+      email: "jamesclark@example.com",
+      password: "J@m3sClark#890",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FJames%20Clark.png?alt=media&token=63e44af6-fab8-45b3-b8c9-e27661c1fe79",
+      user_type: "regular",
+    },
+    {
+      name: "Sophia Harris",
+      username: "sophiaharris77",
+      email: "sophiah@example.com",
+      password: "Soph!aHar7",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FSophia%20Harris.png?alt=media&token=1c747a82-5df9-471c-9a44-3554e4476b44",
+      user_type: "regular",
+    },
+    {
+      name: "Matthew Walker",
+      username: "mattwalker345",
+      email: "matthew.walker@example.com",
+      password: "M@ttWalk345!",
+      avatar_url:
+        "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FMathew%20Walker.png?alt=media&token=3c8958ed-3843-479f-884c-fd39bb135aef",
+      user_type: "regular",
+    },
+  ],
+];
