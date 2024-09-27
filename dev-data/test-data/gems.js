1ff43073a4e3b482a76abe98f0dc5f64b73e3847
espiers13 <167864364+espiers13@users.noreply.github.com>
2024-09-27 16:04:03 +0100

    Added test data via upload

---
+++ dev-data/test-data/gems.js
@@ -0,0 +1,65 @@
+const gems = [
+  {
+    title: "Art in the Park",
+    description:
+      "Local artists display their work in an open park setting with live music and refreshments.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,art%2Din%2Dthe%2Dpark.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
+    address: "1 Piccadilly, Manchester M1 1RG, United Kingdom",
+    date: "2024-10-02T11:00:00",
+    type: "event",
+  },
+  {
+    title: "Foraging Adventure",
+    description:
+      "Join a foraging expert on a walk through nature, learning about what you can eat from nature!",
+    category: "nature",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,foraging%2Dadventure.webp,-Size",
+    ],
+    location: {
+      latitude: 53.40998,
+      longitude: -2.231808,
+    },
+    address: "18 Stenner Ln, Didsbury, Manchester M20 2RQ",
+    date: "2024-10-05T08:00:00",
+    type: "event",
+  },
+  {
+    title: "The Secret Garden Cafe",
+    description:
+      "I stumbled upon this little gem while wandering around the neighborhood. The garden patio is such a peaceful escape from the city, and the coffee? Absolutely the best I've had in a long time! The pastries are homemade and pair perfectly with their artisan brews. I'll definitely be back to enjoy more quiet mornings here.",
+    category: "food",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,secret%2Dgarden.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4642,
+      longitude: -2.2365,
+    },
+    address: "12a Levenshulme Rd, Manchester M19 2BA, United Kingdom",
+    type: "place",
+  },
+  {
+    title: "Moonlit Rooftop Bar",
+    description:
+      "What an amazing find! This rooftop bar is the perfect place to unwind after a busy day. The craft cocktails are incredible, and the view of the city skyline at night is simply breathtaking. It's got a relaxed vibe, and the bartenders were super friendly. A must-visit if you're in the area.",
+    category: "culture",
+    img_url: [
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rooftop%2Dbar.jpg,-Size",
+    ],
+    location: {
+      latitude: 53.4783,
+      longitude: -2.248,
+    },
+    address:
+      "1 Hardman Square, Spinningfields, Manchester M3 3EB, United Kingdom",
+    type: "place",
+  },
+];
---
+++ dev-data/test-data/users.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    name: "John Doe",
+    username: "johndoe123",
+    email: "johndoe@example.com",
+    password: "P@ssw0rd123!",
+    avatar_url:
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FJohn%20Doe.png?alt=media&token=c19e3cb9-6890-498d-b0be-4ec63e24ed11",
+    user_type: "artist",
+  },
+  {
+    name: "Jane Smith",
+    username: "janesmith456",
+    email: "janesmith@example.com",
+    password: "S3cur3P@ss!",
+    avatar_url:
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FJane%20Smith.png?alt=media&token=395e8115-f077-40a8-9731-e2cb44641db9",
+    user_type: "artist",
+  },
+  {
+    name: "Michael Brown",
+    username: "mikebrown789",
+    email: "mikebrown@example.com",
+    password: "Browny2024#",
+    avatar_url:
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FMichael%20Brown.png?alt=media&token=f6821d60-6fd6-4f86-9f81-f7c5ce480898",
+    user_type: "regular",
+  },
+  {
+    name: "Emily Davis",
+    username: "emilyd202",
+    email: "emilydavis@example.com",
+    password: "Eml!DavS9987",
+    avatar_url:
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/avatars%2FEmily%20Davis.png?alt=media&token=6ebd67b9-ac94-4323-94c5-5c9390f886ea",
+    user_type: "regular",
+  },
+];
