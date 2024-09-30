57e2d102e92392b072055414119b4de212b5f3af
Emily <e.spiers13@gmail.com>
2024-09-30 09:43:32 +0100

    added user_id

--- test-data/gems.js
+++ test-data/gems.js
@@ -5,7 +5,7 @@ const gems = [
       "Local artists display their work in an open park setting with live music and refreshments.",
     category: "culture",
     img_url: [
-      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,art%2Din%2Dthe%2Dpark.jpg,-Size",
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fart-in-the-park.jpg?alt=media&token=4d8751d5-78d2-48c8-8e76-7880f1aada24",
     ],
     location: {
       latitude: 53.4808,
@@ -13,6 +13,7 @@ const gems = [
     },
     address: "1 Piccadilly, Manchester M1 1RG, United Kingdom",
     date: "2024-10-02T11:00:00",
+    user_id: 1,
     type: "event",
   },
   {
@@ -21,7 +22,7 @@ const gems = [
       "Join a foraging expert on a walk through nature, learning about what you can eat from nature!",
     category: "nature",
     img_url: [
-      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,foraging%2Dadventure.webp,-Size",
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fforaging-adventure.webp?alt=media&token=5799c51d-0062-457d-9be9-26e2789b76b2",
     ],
     location: {
       latitude: 53.40998,
@@ -29,6 +30,7 @@ const gems = [
     },
     address: "18 Stenner Ln, Didsbury, Manchester M20 2RQ",
     date: "2024-10-05T08:00:00",
+    user_id: 1,
     type: "event",
   },
   {
@@ -37,13 +39,14 @@ const gems = [
       "I stumbled upon this little gem while wandering around the neighborhood. The garden patio is such a peaceful escape from the city, and the coffee? Absolutely the best I've had in a long time! The pastries are homemade and pair perfectly with their artisan brews. I'll definitely be back to enjoy more quiet mornings here.",
     category: "food",
     img_url: [
-      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,secret%2Dgarden.jpg,-Size",
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fsecret-garden.jpg?alt=media&token=bc3795cc-24e5-46fb-bddd-21a1c1c9bf1f",
     ],
     location: {
       latitude: 53.4642,
       longitude: -2.2365,
     },
     address: "12a Levenshulme Rd, Manchester M19 2BA, United Kingdom",
+    user_id: 3,
     type: "place",
   },
   {
@@ -52,7 +55,7 @@ const gems = [
       "What an amazing find! This rooftop bar is the perfect place to unwind after a busy day. The craft cocktails are incredible, and the view of the city skyline at night is simply breathtaking. It's got a relaxed vibe, and the bartenders were super friendly. A must-visit if you're in the area.",
     category: "culture",
     img_url: [
-      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rooftop%2Dbar.jpg,-Size",
+      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Frooftop-bar.jpg?alt=media&token=85c35ee4-006c-4f20-922f-e4e84b205731",
     ],
     location: {
       latitude: 53.4783,
@@ -60,6 +63,7 @@ const gems = [
     },
     address:
       "1 Hardman Square, Spinningfields, Manchester M3 3EB, United Kingdom",
+    user_id: 4,
     type: "place",
   },
 ];
