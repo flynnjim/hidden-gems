c9834936bbcff5ee930439624c052ef7cd1e6e10
flynnjim <flynnlomas@hotmail.co.uk>
2024-09-30 12:18:42 +0100

    data updated to timestamps

--- db/data/dev-data/gems.js
+++ db/data/dev-data/gems.js
@@ -7,12 +7,12 @@ module.exports = [
     img_url: [
       "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fculture-festival.jpg?alt=media&token=3ccefa14-e774-4f77-b39b-ca08e15e23dd",
     ],
-    latitude: 53.4808,
-    longitude: -2.2426,
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
     address: "Piccadilly Gardens, Manchester M1 1RN, United Kingdom",
-    date: "2024-10-15T14:00:00",
-    user_id: 1,
-    rating: [],
+    date: 1697378400000, 
     type: "event",
   },
   {
@@ -21,14 +21,14 @@ module.exports = [
       "Local artists display their work in an open park setting with live music and refreshments.",
     category: "culture",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fart-in-the-park.jpg?alt=media&token=4d8751d5-78d2-48c8-8e76-7880f1aada24",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,art%2Din%2Dthe%2Dpark.jpg,-Size",
     ],
-    latitude: 53.4808,
-    longitude: -2.2426,
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
     address: "Piccadilly Gardens, Manchester M1 1RN, United Kingdom",
-    date: "2024-10-02T11:00:00",
-    user_id: 2,
-    rating: [],
+    date: 1696243200000, 
     type: "event",
   },
   {
@@ -37,14 +37,14 @@ module.exports = [
       "Sample a variety of street food from local and international vendors.",
     category: "food",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fstreet-food.jpeg?alt=media&token=0ce62b67-b908-4a75-9d2f-fd97fcc3b342",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,street%2Dfood.jpeg,-Size",
     ],
-    latitude: 51.5074,
-    longitude: -0.1278,
+    location: {
+      latitude: 51.5074,
+      longitude: -0.1278,
+    },
     address: "Albert Square, Manchester M2 5DB, United Kingdom",
-    date: "2024-09-30T12:00:00",
-    user_id: 3,
-    rating: [],
+    date: 1696070400000, 
     type: "event",
   },
   {
@@ -53,14 +53,14 @@ module.exports = [
       "Join a foraging expert on a walk through nature, learning about what you can eat from nature!",
     category: "nature",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fforaging-adventure.webp?alt=media&token=5799c51d-0062-457d-9be9-26e2789b76b2",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,foraging%2Dadventure.webp,-Size",
     ],
-    latitude: 53.4693,
-    longitude: -2.2672,
+    location: {
+      latitude: 53.4693,
+      longitude: -2.2672,
+    },
     address: "Fletcher Moss Park, Didsbury, Manchester M20 2SW, United Kingdom",
-    date: "2024-10-05T08:00:00",
-    user_id: 4,
-    rating: [],
+    date: 1696521600000, 
     type: "event",
   },
   {
@@ -69,14 +69,14 @@ module.exports = [
       "An exclusive evening of gourmet food tastings from local chefs.",
     category: "food",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fgormet-tasting.jpg?alt=media&token=4641e48e-c5b6-41f4-91f1-06b91377e7c1",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,gormet%2Dtasting.jpg,-Size",
     ],
-    latitude: 53.4783,
-    longitude: -2.244,
+    location: {
+      latitude: 53.4783,
+      longitude: -2.244,
+    },
     address: "The Ivy, Hardman St, Manchester M3 3EB, United Kingdom",
-    date: "2024-10-12T19:00:00",
-    user_id: 5,
-    rating: [],
+    date: 1697131200000, 
     type: "event",
   },
   {
@@ -85,14 +85,14 @@ module.exports = [
       "Capture stunning photos of the city at night with photography expert guidance.",
     category: "culture",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fcity-lights.jpg?alt=media&token=8f220250-30d9-4f8f-bb77-b53180feb019",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,city%2Dlights.jpg,-Size",
     ],
-    latitude: 53.4808,
-    longitude: -2.2426,
+    location: {
+      latitude: 53.4808,
+      longitude: -2.2426,
+    },
     address: "Northern Quarter, Manchester M4 1BD, United Kingdom",
-    date: "2024-09-29T18:30:00",
-    user_id: 6,
-    rating: [],
+    date: 1696006200000, 
     type: "event",
   },
   {
@@ -101,15 +101,15 @@ module.exports = [
       "Learn how to cook dishes from around the world with local chefs.",
     category: "food",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fworld-cuisine.webp?alt=media&token=2bab2d9a-957a-4dcb-b8fe-13bea1f9b867",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,world%2Dcuisine.webp,-Size",
     ],
-    latitude: 53.4832,
-    longitude: -2.2241,
+    location: {
+      latitude: 53.4832,
+      longitude: -2.2241,
+    },
     address:
       "Cookery School, St Peter's Square, Manchester M2 3AE, United Kingdom",
-    date: "2024-10-08T15:00:00",
-    user_id: 7,
-    rating: [],
+    date: 1696779600000, 
     type: "event",
   },
   {
@@ -118,13 +118,14 @@ module.exports = [
       "I stumbled upon this little gem while wandering around the neighborhood. The garden patio is such a peaceful escape from the city, and the coffee? Absolutely the best I've had in a long time! The pastries are homemade and pair perfectly with their artisan brews. I'll definitely be back to enjoy more quiet mornings here.",
     category: "food",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fsecret-garden.jpg?alt=media&token=bc3795cc-24e5-46fb-bddd-21a1c1c9bf1f",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,secret%2Dgarden.jpg,-Size",
     ],
-    latitude: 53.4654,
-    longitude: -2.2367,
+    location: {
+      latitude: 53.4654,
+      longitude: -2.2367,
+    },
     address: "West Didsbury, Manchester M20 2BA, United Kingdom",
-    user_id: 8,
-    rating: [],
+    date: null, 
     type: "place",
   },
   {
@@ -133,14 +134,15 @@ module.exports = [
       "What an amazing find! This rooftop bar is the perfect place to unwind after a busy day. The craft cocktails are incredible, and the view of the city skyline at night is simply breathtaking. It's got a relaxed vibe, and the bartenders were super friendly. A must-visit if you're in the area.",
     category: "culture",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Frooftop-bar.jpg?alt=media&token=85c35ee4-006c-4f20-922f-e4e84b205731",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rooftop%2Dbar.jpg,-Size",
     ],
-    latitude: 53.4783,
-    longitude: -2.244,
+    location: {
+      latitude: 53.4783,
+      longitude: -2.244,
+    },
     address:
       "Hardman Square, Spinningfields, Manchester M3 3EB, United Kingdom",
-    user_id: 8,
-    rating: [],
+    date: null, 
     type: "place",
   },
   {
@@ -149,13 +151,14 @@ module.exports = [
       "This place blew me away! Tucked behind an unassuming bookstore, The Hidden Lantern is a speakeasy like no other. The ambiance is intimate, and the cocktails are unique and carefully crafted. If you love discovering secret spots, you have to check this out. It's like stepping into a different world.",
     category: "culture",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fhidden-lantern.jpeg?alt=media&token=e6b63ee6-f1c1-4055-b0d2-a8fb9b3ae2b6",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,hidden%2Dlantern.jpeg,-Size",
     ],
-    latitude: 53.4811,
-    longitude: -2.2433,
+    location: {
+      latitude: 53.4811,
+      longitude: -2.2433,
+    },
     address: "Royal Exchange, Manchester M2 7EA, United Kingdom",
-    user_id: 8,
-    rating: [],
+    date: null, 
     type: "place",
   },
   {
@@ -164,13 +167,14 @@ module.exports = [
       "This record store is a dream for any music lover! It's a little tucked away, but once you find it, you'll want to stay for hours. They have a great collection of rare vinyls, and the staff is super knowledgeable and friendly. I found some real treasures here, and I can't wait to come back!",
     category: "culture",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Fvinyl-dreams.jpg?alt=media&token=80f1e213-2284-4bdf-bf26-33fde22905b3",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,vinyl%2Ddreams.jpg,-Size",
     ],
-    latitude: 53.4712,
-    longitude: -2.248,
+    location: {
+      latitude: 53.4712,
+      longitude: -2.248,
+    },
     address: "Smithfield Market, Manchester M4 1BN, United Kingdom",
-    user_id: 10,
-    rating: [],
+    date: null, 
     type: "place",
   },
   {
@@ -179,13 +183,14 @@ module.exports = [
       "I came across this charming little farm stand on a weekend drive, and it's now my favorite spot for fresh produce. Everything is organic, and you can tell it's grown with love. The homemade jams are also incredible - I can't leave without grabbing a jar or two!",
     category: "nature",
     img_url: [
-      "https://firebasestorage.googleapis.com/v0/b/fir-project-28217.appspot.com/o/gems%2Frustic-roots.jpg?alt=media&token=6976ce07-dc87-4be9-9168-160a4c9efefa",
+      "https://console.firebase.google.com/project/fir-project-28217/storage/fir-project-28217.appspot.com/files/~2Fgems#:~:text=Name-,rustic%2Droots.jpg,-Size",
     ],
-    latitude: 53.4536,
-    longitude: -2.2684,
+    location: {
+      latitude: 53.4536,
+      longitude: -2.2684,
+    },
     address: "Lymm, Manchester WA13 0HS, United Kingdom",
-    user_id: 11,
-    rating: [],
+    date: null, 
     type: "place",
   },
 ];
