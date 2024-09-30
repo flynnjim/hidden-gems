2f4fe8f326b9951899ce7f52c8ef402872982685
Emily <e.spiers13@gmail.com>
2024-09-30 16:42:49 +0100

    completed listen.js and started app.js

---
+++ listen.js
@@ -0,0 +1,9 @@
+const app = require("./app.js");
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log(`Listening on ${PORT}...`);
+});
