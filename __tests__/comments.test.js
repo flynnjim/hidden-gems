c5d3c5bbfa3722ac42e781bbcac6df222a35bc10
flynnjim <flynnlomas@hotmail.co.uk>
2024-10-01 11:09:35 +0100

    GET comments by gem_id complete with 400 and 404 error handling

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -26,4 +26,41 @@ describe("comments", () => {
         });
     });
   });
+  describe("GET /api/comments/:gem_id", () => {
+    test("200 status code and return specific comments by passed gem_id", () => {
+      return request(app)
+        .get("/api/comments/1")
+        .expect(200)
+        .then((response) => {
+          const { body } = response;
+          expect(Array.isArray(body)).toBe(true);
+          body.forEach((comment) => {
+            expect(comment).toHaveProperty("username");
+            expect(comment).toHaveProperty("body");
+            expect(comment).toHaveProperty("gem_id");
+            expect(comment).toHaveProperty("date");
+            expect(comment).toHaveProperty("comment_id");
+            expect(comment.gem_id).toBe(1);
+          });
+        });
+    });
+    test("returns 400 response and err message when passed invalid gem_id", () => {
+      return request(app)
+        .get("/api/comments/one")
+        .expect(400)
+        .then((response) => {
+          const { body } = response;
+          expect(body).toEqual('Bad request invalid gem_id');
+        });
+    });
+    test("returns 404 response and err message when gem_id not found", () => {
+      return request(app)
+      .get('/api/comments/999')
+      .expect(404)
+      .then((response) => {
+        const { body } = response
+        expect(body).toEqual('Bad request comment not found');
+    })
+    })
+  });
 });
--- app.js
+++ app.js
@@ -6,7 +6,10 @@ app.use(express.json());
 app.use(cors());
 
 const { getAllComments } = require("./controllers/get-all-comments.controller");
+const { getGemComments } = require('./controllers/get-gem-comments.controller')
 
 app.get("/api/comments", getAllComments);
+
+app.get('/api/comments/:gem_id', getGemComments)
 
 module.exports = app;
